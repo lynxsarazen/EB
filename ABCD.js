17756ef5ceecb088474ff4ef28cb013392c734dc
lynxsarazen <47330376+lynxsarazen@users.noreply.github.com>
2019-02-06 19:03:50 +0100

    Delete ABCD.js

--- ABCD.js
+++
@@ -1,7 +0,0 @@
-var div  = document.createElement("DIV");
-div.setAttribute("id","ispis");
-
-document.write(div);
-
-var komentar = document.createComment(" ovo je komentar");
-document.getElementById('ispis') .appendChild(komentar);
