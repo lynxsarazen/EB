f1d636861b0250bbecdf045c25690c2da57672b0
lynxsarazen <47330376+lynxsarazen@users.noreply.github.com>
2019-02-06 19:04:08 +0100

    Delete ABCDE.js

--- ABCDE.js
+++
@@ -1,6 +0,0 @@
-//kolacici se koriste samo ako idemo na isti domen, brisu se 
-var d = new Date ();
-d.setTime(d.getTime() + (5*24*60*60*1000));
-var cookie ="lecturer=Ucha;expires=" + d.toUTCString();
-document.cookie = cookie;
-
